54b4801f17aef3fa5a948a78cdf15879afcf42e7
olumidemixx <OLUMIDEMIXX@GMAIL.COM>
2025-01-24 23:38:24 +0100

    Update MultipleSmartTradersWithFlaskAndRenderChannels.py

--- MultipleSmartTradersWithFlaskAndRenderChannels.py
+++ MultipleSmartTradersWithFlaskAndRenderChannels.py
@@ -28,7 +28,7 @@ BOT_TOKEN = "7931001355:AAFtun7fy35zwg5z_1T_ugoln1f5zdM5Sa0"#"7327291802:AAFPM91
 API_ID = 21202746#int(os.getenv("API_ID"))
 API_HASH = "e700432294937e6925a83149ee7165a0"#os.getenv("API_HASH")
 # Create Telethon client
-telethon_client = TelegramClient('piped', API_ID, API_HASH)
+telethon_client = TelegramClient('gold', API_ID, API_HASH)
 
 # Excluded token address
 EXCLUDED_TOKEN = 'So11111111111111111111111111111112'
